35f3d44093c1d6ac1cb7db0af45c81865a4008b3
sattamap <sattamap@gmail.com>
2024-04-17 21:28:36 +0600

    feat: Enable user registration with Firebase

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,15 +1,24 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import PropTypes from 'prop-types';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
-
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+    const [loading, setLoading] = useState(true);
 
+
+    const createUser = (email, password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
 
 
     const authInfo = {
-  
+        loading,
+        createUser,
     };
 
     return (
