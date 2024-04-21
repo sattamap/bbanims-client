4152540433ddaff1b117ede910a343c76b6ce705
sattamap <sattamap@gmail.com>
2024-04-22 01:23:23 +0600

    feat: Add routes for coordinator, monitor, and user roles in routes.jsx
    - Added route for 'addItems' to render AddItems component for coordinators
    - Added route for 'items' to render Items component for monitors
    - Added route for 'none' to render WelcomeMsg component for users without a specific role

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,6 +6,9 @@ import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import Dashboard from "../layout/Dashboard";
 import AllUsers from "../pages/Dashboard/Admin/AllUsers/AllUsers";
+import AddItems from "../pages/Dashboard/Coordinator/AddItems/AddItems";
+import Items from "../pages/Dashboard/Monitor/Items/Items";
+import WelcomeMsg from "../pages/Dashboard/NoRole/WelcomeMsg.jsx/WelcomeMsg";
 
 export const router = createBrowserRouter([
   {
@@ -35,6 +38,24 @@ export const router = createBrowserRouter([
         element: <AllUsers></AllUsers>,
       },
      
+      // coordinator routes
+     
+      {
+        path: "addItems",
+        element:<AddItems></AddItems>,
+      },
+      
+      // monitor routes
+     
+      {
+        path: "items",
+        element:<Items></Items>,
+      },
+       // user (no role) routes
+       {
+          path: "none",
+          element: <WelcomeMsg></WelcomeMsg>,
+        },
       
 
       
