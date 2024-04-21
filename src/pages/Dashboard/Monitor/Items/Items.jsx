d5b413aaab20560f29eda52f413374321fa28ade
sattamap <sattamap@gmail.com>
2024-04-22 01:17:56 +0600

    feat: Create Items page and WelcomeMsg page in Dashboard

---
+++ src/pages/Dashboard/Monitor/Items/Items.jsx
@@ -0,0 +1,11 @@
+
+
+const Items = () => {
+    return (
+        <div>
+            <h2>Items are</h2>
+        </div>
+    );
+};
+
+export default Items;
---
+++ src/pages/Dashboard/NoRole/WelcomeMsg.jsx/WelcomeMsg.jsx
@@ -0,0 +1,11 @@
+
+
+const WelcomeMsg = () => {
+    return (
+        <div>
+            <h2>You have no role now.</h2>
+        </div>
+    );
+};
+
+export default WelcomeMsg;
