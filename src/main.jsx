7abe58ccc35e533ba151b77718f0e97aed661544
sattamap <sattamap@gmail.com>
2024-04-24 00:39:27 +0600

    feat: Update AllUsers component with data fetching and enhanced UI
    - Integrated useQuery from @tanstack/react-query for fetching user data
    - Utilized useAxiosPublic hook to make API requests to the '/users' endpoint
    - Added loading state for better user experience during data fetching
    - Updated the component to display user information including name, email, and status in a table
    - Added action buttons for viewing user info and deleting users
    - Improved the component's styling and overall structure

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/routes'
 import AuthProvider from './provider/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
         <RouterProvider router={router} />
     </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/pages/Dashboard/Admin/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/Admin/AllUsers/AllUsers.jsx
@@ -1,11 +1,78 @@
 
+import { useQuery } from "@tanstack/react-query";
+import {  FaTrash } from "react-icons/fa";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+
+
+
 
 const AllUsers = () => {
-    return (
-        <div>
-            <h2>Users 2</h2>
-        </div>
-    );
+  const axiosPublic = useAxiosPublic();
+ 
+
+  const { data: users = [], isLoading} = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/users");
+      return res.data;
+    }
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+ 
+  return (
+    <div>
+      <div className="flex justify-evenly bg-emerald-800 p-10 rounded-xl">
+        <h2 className="text-2xl text-slate-100 font-extrabold">All Users</h2>
+        <h2 className="text-2xl text-slate-100 font-extrabold">Total users: {users.length}</h2>
+      </div>
+      <div className="overflow-x-auto px-20">
+        <table className="table table-xs table-zebra mt-10 ">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Status</th>
+              <th>User Info</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+ role
+</td>
+
+  
+                <td>
+                  <button
+                    className="btn btn-ghost btn-xs"
+                  >
+                    See Info
+                  </button>
+                </td>
+                <th>
+                  <button
+                    className="btn btn-ghost btn-xs"
+                  >
+                    <FaTrash></FaTrash>
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
