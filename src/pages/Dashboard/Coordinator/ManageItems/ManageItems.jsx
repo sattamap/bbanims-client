79fa3f1a2fd6185f3ab984a13509a63598716a67
sattamap <sattamap@gmail.com>
2024-04-24 02:34:51 +0600

    feat: Add ManageItems component for managing items
    - Created ManageItems component to fetch and display items from API
    - Utilized useAxiosPublic to fetch items and store them in state
    - Displayed items in a table with columns for name, image, condition, quantity, date, detail, and actions
    - Provided actions for each item: delete and edit
    - Improved user experience for managing items

--- src/pages/Dashboard/Coordinator/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/Coordinator/ManageItems/ManageItems.jsx
@@ -1,11 +1,91 @@
+import { useEffect, useState } from "react";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import { Link } from "react-router-dom";
+
 
 
 const ManageItems = () => {
-    return (
-        <div>
-            <h2>This is Manage Items page</h2>
-        </div>
-    );
+  const axiosPublic = useAxiosPublic();
+  const [items, setItems] = useState([]);
+
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await axiosPublic.get('/items');
+        setItems(response.data);
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
+    };
+
+    fetchItems();
+  }, [axiosPublic]);
+
+
+
+
+
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table table-xs">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name,Image & Condition</th>
+              <th>Quantity</th>
+              <th>Date of Receive</th>
+              <th>Detail</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}.</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={item?.image} alt={item?.itemName} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{item?.itemName}</div>
+                      <div className="text-sm opacity-50">{item?.condition}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {item?.quantity}
+                </td>
+                <td>{item?.date}</td>
+                <td>
+                  <p className="">{item?.detail}</p>
+                </td>
+                <th>
+                  <div className="flex gap-2">
+                    <button
+                      className="btn btn-warning btn-xs"
+                    >
+                      Delete
+                    </button>
+                    <Link to='\'>
+                      <button className="btn btn-neutral btn-xs">Edit</button>
+                    </Link>
+                  </div>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+
+    </div>
+  );
 };
 
 export default ManageItems;
