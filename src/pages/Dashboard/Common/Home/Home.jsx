c066de09504e4aa3e02a1efeb572ecf64991b443
sattamap <sattamap@gmail.com>
2024-05-04 18:22:50 +0600

    feat: Create Home component and use it in CoordinatorHome
    - Created a new Home component with buttons for Equipment and Parts
    - Integrated the Home component into CoordinatorHome for consistent user interface
    - Improved code organization by reusing the Home component in CoordinatorHome

---
+++ src/pages/Dashboard/Common/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div className="flex flex-row gap-10">
+           <button className="btn p-16 btn-lg btn-accent">Equipment</button>
+           <button className="btn p-16 btn-lg btn-accent">Parts</button>
+        </div>
+    );
+};
+
+export default Home;
--- src/pages/Dashboard/Coordinator/CoordinatorHome/CoordinatorHome.jsx
+++ src/pages/Dashboard/Coordinator/CoordinatorHome/CoordinatorHome.jsx
@@ -1,10 +1,10 @@
+import Home from "../../Common/Home/Home";
 
 const CoordinatorHome = () => {
     return (
-        <div className="flex flex-row gap-10">
-           <button className="btn p-16 btn-lg btn-accent">Equipment</button>
-           <button className="btn p-16 btn-lg btn-accent">Parts</button>
-        </div>
+      <div>
+        <Home></Home>
+      </div>
     );
 };
 
