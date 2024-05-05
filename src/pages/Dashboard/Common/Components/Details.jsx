1d9a268308ead77a59359741d611235f4b1db889
sattamap <sattamap@gmail.com>
2024-05-05 14:01:21 +0600

    Add css for better design in Details.jsx

--- src/pages/Dashboard/Common/Components/Details.jsx
+++ src/pages/Dashboard/Common/Components/Details.jsx
@@ -25,20 +25,22 @@ const Details = () => {
             <img src={itemDetails?.image} className="w-full rounded-lg shadow-2xl" alt={itemDetails?.birdNameENG} />
             <div className="lg:w-2/3 mt-6 lg:mt-0">
               <h1 className="text-base font-medium mb-2 lg:mb-4">
-                Name of the Item <span className="lg:mr-2">:</span>
+                Name of the Item <span className="lg:ml-[86px] lg:mr-2">:</span>
                 <span className="text-lg font-bold block xl:inline ">{itemDetails?.itemName}</span>
               </h1>
               <h1 className="text-base font-medium mb-2 lg:mb-4">
-                Location <span className="lg:ml-16 lg:mr-2">:</span>
+                Location <span className="lg:ml-[155px] lg:mr-2">:</span>
                 <span className="text-lg font-bold block xl:inline">{itemDetails?.location}</span>
               </h1>
               <h1 className="text-base font-medium mb-2 lg:mb-4">
-                Date of Receive/Installation <span className="lg:ml-24 lg:mr-2">:</span>
+                Date of Receive/Installation <span className="lg:ml-4 lg:mr-2">:</span>
                 <span className="text-lg font-bold block xl:inline">{itemDetails?.date}</span>
               </h1>
               <h1 className="text-base font-medium mb-2 lg:mb-4">
-                Condition <span className="lg:ml-4 lg:mr-2">:</span>
-                <span className="text-lg font-bold block xl:inline">{itemDetails?.condition}</span>
+                Condition <span className="lg:ml-36 lg:mr-2">:</span>
+                <span className={`${
+            itemDetails?.condition === "Good" ? "bg-green-300 p-1 text-lg font-bold block xl:inline" : "bg-red-400 p-1 px-2 text-lg font-bold block xl:inline"
+        } p-2 rounded`}>{itemDetails?.condition}</span>
               </h1>
               <p className="text-justify overflow-auto max-h-64">{itemDetails?.detail}</p>
             </div>
